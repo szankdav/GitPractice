2d4d245146f0d79d3b1acb26689826744ff2b392
Dávid Szankovszky <szankovszky.david@gmail.com>
2024-06-19 16:02:34 +0200

    Add project files.

---
+++ GitPractice/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace GitPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Szia! Kérlek add meg a nevedet: ");
+            string nev = Console.ReadLine();
+            Console.Write("Kérlek add meg az életkorodat: ");
+            int kor;
+            while (int.TryParse(Console.ReadLine(), out kor) == false)
+            {
+                Console.Write("Kérlek csak számot írj be: ");
+            }
+            Console.WriteLine($"Szia {nev}! Az általad megadott életkor: {kor}");
+
+            Console.ReadKey();
+        }
+    }
+}
